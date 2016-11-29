f23cd933930849714e390c82fbf7008996572ba9
mboldyrev <miboldyrev@gmail.com>
2016-11-29 16:42:28 +0300

    cleanup

--- read/emes_labs/lab5_emes/TWRP.custom-recovery/start-2.8.0.1.bat
+++
@@ -1,4 +0,0 @@
-REM http://stackoverflow.com/questions/5034076/what-does-dp0-mean-and-how-does-it-work
-cd /d %~dp0
-C:\android-sdk-windows\tools\emulator -avd TWRP -ramdisk twrp-2.8.0.1-twrp.img -kernel goldfish_2.6_kernel
-pause
--- read/emes_labs/lab5_emes/busybox/readme.txt
+++
@@ -1,1 +0,0 @@
-check out [server]\embedded_forensics\Android-tools\busybox.net
